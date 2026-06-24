2f8bc1491de931571abc55f880a8801ebfa16147
eshapatel2005 <esha.patel5605@gmail.com>
2026-06-24 15:07:22 +0530

    Toggle Mode : Black & White

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -5,17 +5,17 @@ function Home() {
   const { students } = useContext(StudentContext);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="min-h-screen p-6">
+      {" "}
       {/* Header */}
       <div className="mb-6">
-        <h1 className="text-4xl font-bold text-gray-800">
+        <h1 className="text-4xl font-bold text-white-800">
           Student Dashboard 📊
         </h1>
-        <p className="text-gray-600 mt-1">
+        <p className="text-white-600 mt-1">
           Manage and track all student records in one place
         </p>
       </div>
-
       {/* Stats Cards */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-8">
         <div className="bg-gradient-to-r from-blue-500 to-blue-700 text-white p-6 rounded-xl shadow-lg">
@@ -33,10 +33,9 @@ function Home() {
           <p className="text-4xl font-bold mt-2">{students.length}</p>
         </div>
       </div>
-
       {/* Recent Students Section */}
-      <div className="bg-white p-6 rounded-xl shadow">
-        <h2 className="text-2xl font-bold mb-4">Recent Students</h2>
+      <div className="bg-white p-6 rounded-xl shadow text-black">
+        <h2 className="text-2xl font-bold mb-4 ">Recent Students</h2>
 
         {students.length === 0 ? (
           <p className="text-gray-500">
